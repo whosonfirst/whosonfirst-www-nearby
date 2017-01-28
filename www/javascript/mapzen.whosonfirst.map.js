383b7b9792b37eecc794296c4c042edd3903e85b
thisisaaroland <aaron@mapzen.com>
2017-01-27 18:13:03 -0800

    import all the whosonfirstjs

--- www/javascript/mapzen.whosonfirst.map.js
+++ www/javascript/mapzen.whosonfirst.map.js
@@ -9,14 +9,26 @@ mapzen.whosonfirst.map = (function(){
 			
 		'init': function(){
 
+			var lat = 37.7749
+			var lon = -122.4194;
+			var zoom = 12;
+
+			var hash = location.hash;
+			var match = hash.match(/^\#\d+\/(-?\d+(?:\.\d+))?\/(-?\d+(?:\.\d+))?/)
+						
+			if (match){
+				lat = match[1];
+				lon = match[2];
+			}
+
 			var opts = {
 				tangramOptions: { 
 					scene: L.Mapzen.BasemapStyles.Refill
 				}
 			};
-			
+
 			var map = L.Mapzen.map('map', opts);
-			map.setView([37.7749, -122.4194], 12);
+			map.setView([lat, lon], zoom);
 
 			var geocoder = L.Mapzen.geocoder('mapzen-xxxxxxx');
 			geocoder.addTo(map);
@@ -40,9 +52,16 @@ mapzen.whosonfirst.map = (function(){
 				controls.style.display = "inline";
 				
 				slippymap.crosshairs.init(map);
+				
 			});
-			
+
+			if ((mapzen.whosonfirst.iplookup) && (mapzen.whosonfirst.iplookup.enabled())){
+
+				mapzen.whosonfirst.map.iplookup.init(map);
+				mapzen.whosonfirst.map.iplookup.lookup();
+			}		
 		},
+
 	};		
 
 	return self;
