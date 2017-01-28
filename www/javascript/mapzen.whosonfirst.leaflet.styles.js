069784ccbc6a4ae94770726cc292511474d005ce
thisisaaroland <aaron@mapzen.com>
2017-01-27 19:58:34 -0800

    css for log stuff; import leaflet styles and handlers

---
+++ www/javascript/mapzen.whosonfirst.leaflet.styles.js
@@ -0,0 +1,167 @@
+var mapzen = mapzen || {};
+mapzen.whosonfirst = mapzen.whosonfirst || {};
+mapzen.whosonfirst.leaflet = mapzen.whosonfirst.leaflet || {};
+
+mapzen.whosonfirst.leaflet.styles = (function(){
+
+	var self = {
+
+		'bbox': function(){
+			return {
+				"color": "#000000",
+				"weight": .5,
+				"opacity": 1,
+				"fillColor": "#000000",
+				"fillOpacity": .4,
+			};
+		},
+
+		'label_centroid': function(){
+
+			return {
+				"color": "#fff",
+				"weight": 3,
+				"opacity": 1,
+				"radius": 10,
+				"fillColor": "#ff0099",
+				"fillOpacity": 0.8
+			};
+		},
+
+		'math_centroid': function(){
+
+			return {
+				"color": "#fff",
+				"weight": 2,
+				"opacity": 1,
+				"radius": 6,
+				"fillColor": "#ff7800",
+				"fillOpacity": 0.8
+			};
+		},
+
+		'geom_centroid': function(){
+
+			return {
+				"color": "#fff",
+				"weight": 3,
+				"opacity": 1,
+				"radius": 10,
+				"fillColor": "#32cd32",
+				"fillOpacity": 0.8
+			};
+		},
+
+		'search_centroid': function(){
+
+			return {
+				"color": "#000",
+				"weight": 2,
+				"opacity": 1,
+				"radius": 6,
+				"fillColor": "#0BBDFF",
+				"fillOpacity": 1
+			};
+		},
+
+		'breach_polygon': function(){
+
+			return {
+				"color": "#ffff00",
+				//"color": "#002EA7",
+				"weight": 1.5,
+				"dashArray": "5, 5",
+				"opacity": 1,
+				"fillColor": "#002EA7",
+				"fillOpacity": 0.1
+			};
+		},
+
+		'consensus_polygon': function(){
+
+			return {
+				"color": "#ff0066",
+				"weight": 2,
+				"opacity": 1,
+				"fillColor": "#ff69b4",
+				"fillOpacity": 0.6
+			};
+		},
+
+		'parent_polygon': function(){
+
+			return {
+				"color": "#000",
+				"weight": 1,
+				"opacity": 1,
+				"fillColor": "#00308F",
+				"fillOpacity": 0.5
+			};
+		},
+
+		'venue_current': function(){
+			return {
+				"color": "#398439",
+				"weight": 1,
+				"opacity": 1,
+				"radius": 4,
+				"fillColor": "#5cb85c",
+				"fillOpacity": 0.5
+			};
+		},
+
+		'venue_not_current': function(){
+			return {
+				"color": "#ec971f",
+				"weight": 1,
+				"opacity": 1,
+				"radius": 4,
+				"fillColor": "#f0ad4e",
+				"fillOpacity": 0.5
+			};
+		},
+
+		'venue_deprecated': function(){
+			return {
+				"color": "#ac2925",
+				"weight": 1,
+				"opacity": 1,
+				"radius": 4,
+				"fillColor": "#d9534f",
+				"fillOpacity": 0.5
+			};
+		},
+
+		'venue_funky': function(){
+			return {
+				"color": "#269abc",
+				"weight": 1,
+				"opacity": 1,
+				"radius": 4,
+				"fillColor": "#5bc0de",
+				"fillOpacity": 0.5
+			};
+		},
+
+		'venue_unknown': function(){
+			return {
+				"color": "#204d74",
+				"weight": 1,
+				"opacity": 1,
+				"radius": 4,
+				"fillColor": "#337ab7",
+				"fillOpacity": 0.5
+			};
+		},
+
+		'venue_hover': function(){
+			return {
+				"radius": 6,
+				"fillOpacity": 1
+			};
+		}
+	};
+
+
+	return self;
+})();
--- www/javascript/mapzen.whosonfirst.nearby.js
+++ www/javascript/mapzen.whosonfirst.nearby.js
@@ -3,11 +3,99 @@ mapzen.whosonfirst = mapzen.whosonfirst || {};
 
 mapzen.whosonfirst.nearby = (function(){
 
+	var drawn = {};
+	
 	var self = {
 			
 		'init': function(){
-			
+
+			var map = mapzen.whosonfirst.map.init();
+			map.on("dragend", self.fetch);
+
+			self.fetch();
 		},
+
+		'fetch': function(){
+
+			var map = mapzen.whosonfirst.map.map_object();
+			var pt = map.getCenter();
+
+			var method = "whosonfirst.places.getNearby";
+			
+			var args = {
+				"latitude": pt.lat,
+				"longitude": pt.lng,
+				"placetype": "venue",
+				"extras": "geom:latitude,geom:longitude",
+			};
+
+			var on_success = function(rsp){
+
+				try {
+					self.draw(rsp);
+				}
+
+				catch (e) {
+					console.log(e);
+				}
+			};
+			
+			mapzen.whosonfirst.api.call(method, args, on_success);
+		},
+
+		'draw': function(rsp) {
+
+			var features = [];
+			
+			var results = rsp.results;
+			var count = results.length;
+
+			for (var i=0; i < count; i++){
+
+				var props = results[i];
+				var wofid = props["wof:id"];
+
+				if (drawn[wofid]){
+					continue;
+				}
+
+				var lat = props["geom:latitude"];
+				var lon = props["geom:longitude"];				
+				
+				var geom = {
+					"type": "Point",
+					"coordinates": [ lon, lat ],
+				};
+
+				var feature = {
+					"type": "Feature",
+					"geometry": geom,
+					"properties": props,
+				};
+				
+				features.push(feature);
+				drawn[wofid] = wofid;
+			}
+
+			var collection = {
+				"type": "FeatureCollection",
+				"features": features,
+			};
+
+			var map = mapzen.whosonfirst.map.map_object();
+
+			var style = mapzen.whosonfirst.leaflet.styles.geom_centroid();
+			var handler = mapzen.whosonfirst.leaflet.handlers.point(style);
+
+			var args = {
+				'style': style,
+				'pointToLayer': handler,
+			};
+			
+			var layer = L.geoJson(collection, args);
+			layer.addTo(map);
+		}
+		
 	};		
 
 	return self;
